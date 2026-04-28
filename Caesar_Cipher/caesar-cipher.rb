1d75cdf705b421c7c8156658ca6ed840f396b4fc
Rick-Astley2 <191059137+Rick-Astley2@users.noreply.github.com>
2026-04-28 14:02:43 -0500

    Forgot uppercase letters
    There is definitly a better way to do this

--- Caesar_Cipher/caesar-cipher.rb
+++ Caesar_Cipher/caesar-cipher.rb
@@ -1,18 +1,28 @@
 def caesar_cipher(texts, shift) 
-  alphabet = [
+  lower_alphabet = [
     "a", "b", "c", "d", "e", "f", "g", 
     "h", "i", "j", "k", "l", "m", "n", 
     "o", "p", "q", "r", "s", "t", "u", 
-    "v", "w", "x", "y", "z",
+    "v", "w", "x", "y", "z"
   ]
+  upper_alphabet = [
+    "A", "B", "C", "D", "E", "F", "G", 
+    "H", "I", "J", "K", "L", "M", "N", 
+    "O", "P", "Q", "R", "S", "T", "U", 
+    "V", "W", "X", "Y", "Z"]
 
   letters = texts.split("")
 
-    shifted = letters.map do |letter| 
-    if alphabet.include?(letter)
-      index = alphabet.find_index(letter)
-      index = (index + shift) % 26
-      alphabet[index]
+  shifted = letters.map do |letter| 
+
+    if lower_alphabet.include?(letter)
+        index = lower_alphabet.find_index(letter)
+        index = (index + shift) % 26
+        lower_alphabet[index]
+    elsif upper_alphabet.include?(letter)
+        index = upper_alphabet.find_index(letter)
+        index = (index + shift) % 26
+        upper_alphabet[index]
     else
       letter
     end
