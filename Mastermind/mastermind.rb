4498f7c1cd2e583687b9f3119b99454f34d4580f
Rick-Astley2 <191059137+Rick-Astley2@users.noreply.github.com>
2026-05-26 13:38:23 -0500

    Got mastermind fully working with the computer choosing the code

--- Mastermind/mastermind.rb
+++ Mastermind/mastermind.rb
@@ -6,7 +6,7 @@ class Board
   attr_accessor :grid
 
   def initialize
-    self.grid = Array.new(12) { Array.new(4) { '_' } } 
+    self.grid = Array.new(12) { Array.new(4) { '_' } }
   end
 
   def display
@@ -21,19 +21,20 @@ end
 
 class Computer
   attr_accessor :code
+
   def code_generator
     colors = %w[r y g p b m]
 
-    return code = colors.sample(4)
-
+    colors.sample(4)
   end
 end
 
 class Player
   attr_accessor :guess
+
   def player_guess
     loop do
-      print 'Guess:'
+      print 'Guess: '
 
       colors = %w[r y g p b m]
 
@@ -57,17 +58,49 @@ class Game
     self.computer = Computer.new
   end
 
+  def win?(guess, code)
+    return true if guess == code
+
+    false
+  end
+
+  def evaluate_guess(guess, code)
+    g = 0
+    w = 0
+
+    guess.each_with_index do |value, index|
+      next unless value == code[index]
+
+      g += 1
+
+      next if value != code[index]
+
+      w += 1 if code.include?(value)
+    end
+
+    [g, w]
+  end
+
   def start
     board.display
     board.select_color
-    
+    times_guessed = 1
+    code = computer.code_generator
 
-
-    loop do 
-      code = computer.code_generator
-      guess = player1.player_guess
-      puts guess
+    (1..12).each do |_|
+      puts "Times guessed: #{times_guessed}."
+      times_guessed += 1
       puts code
+      guess = player1.player_guess
+      g, w  = evaluate_guess(guess, code)
+      puts "Green #{g}"
+      puts "White #{w}"
+      puts guess
+
+      if win?(guess, code)
+        puts "You're now a chad"
+        break
+      end
     end
   end
 end
@@ -79,4 +112,4 @@ end
 
 main
 
-# test: r,r,r,r 
+# test: r,r,r,r
